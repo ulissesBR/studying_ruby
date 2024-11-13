ae6203eb787d8d61380456aafeb07d6bd86faf7f
Ulisses Piassa <ulisses.piassa@gmail.com>
2024-11-12 23:08:16 -0300

    Desafio 1

---
+++ desafio.rb
@@ -0,0 +1,12 @@
+#desafio 1
+
+print"Digite o seu nome: "
+nome = gets.chomp
+
+print"Digite o seu sobrenome: "
+sobrenome = gets.chomp
+
+print"Digite a sua idade: "
+idade = gets.chomp.to_i()
+
+puts "Nome: #{nome}; Sobrenome: #{sobrenome}; Idade #{idade}"
